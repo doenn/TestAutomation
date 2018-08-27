118f2ccfb4b25d97ebdb5011c73e759a53f0a04b
Lewis Sall <lewissall@hotmail.com>
2018-08-27 18:08:55 -0400

    Demo Page

---
+++ src/pages/DemoPage.ts
@@ -0,0 +1,26 @@
+/// <reference path="../steps.d.ts" />
+
+import {BrowserHelper} from "../helpers/BrowserHelper.js";
+import topNavBarPage = require('../pages/TopNavBarPage.js');
+import addNewUserPage = require('../pages/AddNewUserPage.js');
+var I = actor();
+
+let assert = require('assert');
+
+
+export = {
+	demoHeading: "//h3[text()[contains(.,'Prospect List')]]",
+	questionField: "//input[@name='keyword']",
+	recommendedTab: "//ul[@class='nav nav-tabs']//li//a[@id='dod_recommended_header']",
+
+loaded(){
+		I.waitForElement(this.prospectListHeading,20);
+	},
+	searchProspect(searchCriteria : string){
+		I.click(this.searchField);
+		I.fillField(this.searchField, searchCriteria);
+	},
+	
+	
+
+}
