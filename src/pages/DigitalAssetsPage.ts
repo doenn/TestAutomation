85a4bdb716e922184876722e27cbc74d86de594f
Lewis Sall <lewissall@hotmail.com>
2018-08-31 20:17:27 -0400

    Asset rows

--- src/pages/DigitalAssetsPage.ts
+++ src/pages/DigitalAssetsPage.ts
@@ -12,6 +12,9 @@ export = {
 	digitalAssetsHeading: "//h3[text()[contains(.,'Digital Assets')]]",
 	uploadButton: "//input[@id='digital_asset_upload']",
 	searchField: "//input[@type='search']",
+	getAssetRowSelector(rowIndex : number) : string{
+		return "//tbody//tr["+(rowIndex+1)+"]";		// Row index is 1 based
+	},
 	/*questionField: "//input[@name='keyword']",
 	recommendedTab: "//ul[@class='nav nav-tabs']//li//a[@id='dod_recommended_header']",
 	searchResultsTab: "//ul[@class='nav nav-tabs']//li//a[@id='dod_searchresults_header']",
